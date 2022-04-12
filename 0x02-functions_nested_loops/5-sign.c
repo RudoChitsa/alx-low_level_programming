9061094e7b916b91a7c5dcabbfb91c6606b05140
RudoChitsa <47056287+RudoChitsa@users.noreply.github.com>
2022-04-12 13:09:59 +0200

    task 06

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,29 +5,21 @@
  *
  * Return: Always 0.
  */
-int main(void)
-{
-    int r;
-
+int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(43);
 		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar('0');
+		_putchar(48);
 		return (0);
-	}
-	else if (n < 0)
-	{
-		_putchar('-');
-		return (-1);
 	}
 	else
 	{
-		_putchar('-');
-		return ('/');
+		_putchar(45);
+		return (-1);
 	}
 }
