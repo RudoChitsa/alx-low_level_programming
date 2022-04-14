3ac5575f8e1fb595032d273fac9a9fae8596332f
RudoChitsa <47056287+RudoChitsa@users.noreply.github.com>
2022-04-14 12:28:25 +0200

    task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,24 +1,22 @@
 #include "main.h"
-#include "stdio.h"
 /**
- * print_number - takes an integer and prints it with _putchar
- * @n: integer to print
+ * print_number - prints an integer
+ * @n:.input integer parameter
  *
- * Return: void
+ *
  */
 void print_number(int n)
 {
-	// If number is smaller than 0, put a - sign
-    // and change number to positive
-    if (n < 0) {
-        putchar('-');
-        n = -n;
-    }
- 
-    // Remove the last digit and recur
-    if (n/10)
-        print(n/10);
- 
-    // Print the last digit
-    putchar(n%10 + '0');
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		i = -i;
+	}
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar(i % 10 + '0');
 }
