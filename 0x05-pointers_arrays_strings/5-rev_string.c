06e2e07a054cedf2931b2f4dee33dee90f0c5ea8
RudoChitsa <47056287+RudoChitsa@users.noreply.github.com>
2022-04-20 14:13:42 +0200

    task 05

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,26 +7,12 @@
  */
 void rev_string(char *s)
 {
-	int i;
-	int j;
-	int temp;
-	int length;
+	int main(void)
+{
+    char s[10] = "My School";
 
-	while (s[i] != 0)
-	{
-		i++;
-	}
-
-	length = i;
-	i = 0;
-	j = length - 1;
-
-	while (i < j)
-	{
-		temp = s[i];
-		s[i] = s[j];
-		s[j] = temp;
-		i++;
-		j--;
-	}
+    printf("%s\n", s);
+    rev_string(s);
+    printf("%s\n", s);
+    return (0);
 }
