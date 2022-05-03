ebb826c7105fcc4ac9a25e19688bb2d287d3a77f
RudoChitsa <47056287+RudoChitsa@users.noreply.github.com>
2022-05-03 16:00:24 +0200

    task 05

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,78 +1,70 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
+
 
 /**
- * main - Prints minimum number of coins needed to get cents
- * @argc: number of arguments including command name
- * @argv: Pointer to array of pointers to arguments as strings including
- *			command name
- *
- * Return: 0 for success, 1 for error
+ * coinConverter - Helper function that does all the mathematics
+ * @i: Passed in variable from main for calculations
+ * Return: The number of coins needed minimum for the passed in variable
  */
-int main(int argc, char **argv)
+int coinConverter(int i)
 {
-	long int input, coinnum = 0;
-	int coins[5] = {25, 10, 5, 2, 1};
-	int i;
+	int count = 0;
 
-	if (argc != 2)
+	while (i != 0)
 	{
-		puts("Error");
-		return (1);
-	}
-
-	input = atoi(argv[1]);
-
-	if (input < 0)
-	{
-		puts("Error");
-		return (1);
-	}
-
-	for (i = 0; i < 5; i++)
-	{
-		while (input - coins[i] >= 0)
+		if (i % 10 == 9 || i % 10 == 7)
+			i -= 2;
+		else if (i % 25 == 0)
+			i -= 25;
+		else if (i % 10 == 0)
+			i -= 10;
+		else if (i % 5 == 0)
+			i -= 5;
+		else if (i % 2 == 0)
 		{
-			coinnum++;
-			input -= coins[i];
+			if (i % 10 == 6)
+				i -= 1;
+			else
+				i -= 2;
 		}
+		else
+			i -= 1;
+
+		count++;
 	}
 
-	printf("%li\n", coinnum);
-
-	return (0);
+	return (count);
 }
 
 /**
- * atoi - Converts a string to an integer
- * @s: pointer to the first character of the string
- *
- * Return: Value of integer in string
+ * main - Takes in exactly one argument for minimum coin count
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0 if exactly 1 argument is passed into this program, 1 otherwise
  */
-long int atoi(char *s)
+int main(int argc, char *argv[])
 {
-	unsigned int num;
-	int neg;
+	int i, coin;
 
-	neg = 1;
-	num = 0;
+	coin = 0;
 
-	for (; *s; s++)
+	if (argc != 2)
 	{
-		if (*s >= '0' && *s <= '9')
-		{
-			num *= 10;
-			num += *s - '0';
-		}
-		else if (num > 0)
-		{
-			break;
-		}
-		else if (*s == '-')
-		{
-			neg = -neg;
-		}
+		printf("Error\n");
+		return (1);
 	}
 
-	return (num * neg);
+	i = atoi(argv[1]);
+
+	if (i < 0)
+		printf("0\n");
+	else
+	{
+		coin = coinConverter(i);
+
+		printf("%d\n", coin);
+	}
+
+	return (0);
 }
