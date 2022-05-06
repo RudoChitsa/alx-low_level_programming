03ad8a427e3c0eb2d18c06289b9dcbde3c7fe4f4
RudoChitsa <47056287+RudoChitsa@users.noreply.github.com>
2022-05-06 15:15:02 +0200

    task 06

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,212 +4,96 @@
 #include <string.h>
 
 /**
- * add - addition of parcial products
- * @p: parcial product
- * @mg: big malloc
- * @i: size of big malloc - 1
- * @tp: size of parcial product - 1
-  */
-void add(char *p, char *mg, int i, int tp)
-{
-	int pos = tp, a = 0, b = 0, car = 0, res = 0;
-	int cont = tp, aux = 0;
-
-	for (; cont >=  0; cont--, i--, pos--)
-	{
-		a = *(p + pos) - '0';
-		b = *(mg + i) - '0';
-		res = (a + b) + car;
-		car = res / 10;
-		res = (res % 10) + '0';
-		*(mg + i) = res;
-	}
-	res = 0;
-	for (aux = i - 1; aux >= 0; aux--)
-	{
-		a = *(mg + aux) - '0';
-		a += a + car;
-		car = a / 10;
-		a = (a % 10) + '0';
-		*(mg + aux) = a;
-	}
-}
-/**
- * setiar - fill the pointer with 0
- * @s: pointer
- * @a: size of pointer
- */
-void setiar(char *s, int a)
+* _isNum - check if is a number
+*@num: string to check
+*Return: 1 is numm, 0 not num
+*/
+int _isNum(char *num)
 {
 	int i;
 
-	for (i = 0; i < a; i++)
-		s[i] = '0';
-}
-/**
- * parcial - give the parcial malloc
- * @a: size one
- * Return: parcial malloc
- */
-char *parcial(int a)
-{
-	int i;
-	char *p;
-
-	p = malloc(a);
-	if (!p)
-		return (NULL);
-	for (i = 0; i < a; i++)
-		p[i] = '0';
-	return (p);
-}
-/**
- * grande - give big malloc
- * @a: size one
- * @b: size two
- * Return: malloc
- */
-char *grande(int a, int b)
-{
-	int i;
-	char *p;
-
-	p = malloc(a + b);
-	if (!p)
-		return (NULL);
-	for (i = 0; i < a + b; i++)
-		p[i] = '0';
-	return (p);
-}
-/**
- * tama - give the size
- * @s: string of numbers
- * Return: size
- */
-int tama(char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
+	for (i = 0; num[i] != '\0'; i++)
 	{
-		i++;
+		if (num[i] < '0' || num[i] > '9')
+			return (0);
 	}
-	return (i);
+	return (1);
 }
-/**
- * Perror - print Error with putchar.
- * Return: Always 0.
- */
-void Perror(void)
-{
-	_putchar('E');
-	_putchar('r');
-	_putchar('r');
-	_putchar('o');
-	_putchar('r');
-	_putchar('\n');
-	exit(98);
-}
-/**
- * numero - check if strings have only numbers.
- * @s: string 1
- * @a: string 2
- * Return: Always 1 if not or 0 if only has number.
- */
-void numero(char *s, char *a)
-{
-	int i = 0, aux = 0;
 
-	while (s[i] != '\0')
-	{
-		if (!(s[i] >= '0' && s[i] <= '9'))
-		{
-			aux = 1;
-			break;
-		}
-		i++;
-	}
-	i = 0;
-	while (a[i] != '\0')
-	{
-		if (!(a[i] >= '0' && a[i] <= '9'))
-		{
-			aux = 1;
-			break;
-		}
-		i++;
-	}
-	if (aux == 1)
-		Perror();
-	if (*s == '0' || *a == '0')
-	{
-		_putchar('0');
-		_putchar('\n');
-		exit(0);
-	}
-}
 /**
- * impresion - print the pointer
- * @s: string.
- * @a: size of string
- */
-void impresion(char *s, int a)
+* *_memset - copies a character to the firstn characters of the string pointed
+*@s: original string
+*@b: value to remplace
+*@n: number of bytes
+*Return: s (string modify)
+*/
+char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
-	for (i = 0; i < a; i++)
-	{
-		if (!(s[0] == '0' && i == 0))
-			_putchar(s[i]);
-	}
-	_putchar('\n');
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
 }
+
 /**
- * main - check the code for Holberton School students.
- * @argv: array of pointers store the arguments
- * @argc: quantity of arguments
- * Return: Always 0.
- */
+* _strlen - returns the lenght of a string
+*@s: poiter of character
+*Return: the length of a string
+*/
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	while (*(s + len) != '\0')
+		len++;
+	return (len);
+}
+
+/**
+* main - multiple 2 positive numbers
+*@argc: argument counter
+*@argv: number to multiply
+*Return: 0 (success)
+*/
 int main(int argc, char *argv[])
 {
-	int t1, t2, t3, a = 0, b = 0, j = 0, aux = 0, car, g, ind;
-	char *mg, *p, *size1, *size2;
+	int length, c, prod, i, j, l1, l2;
+	int *res;
 
-	if (argc != 3)
-		Perror();
+	if ((argc != 3 || !(_isNum(argv[1]))) || !(_isNum(argv[2])))
+		puts("Error"), exit(98);
+	l1 = _strlen(argv[1]), l2 = _strlen(argv[2]);
+	length = l1 + l2;
+	res = calloc(length, sizeof(int *));
+	if (res == NULL)
+		puts("Error"), exit(98);
+	for (i = l2 - 1; i > -1; i--)
+	{
+		c = 0;
+		for (j = l1; j > -1; j--)
+		{
+			prod = (argv[2][i] - '0') * (argv[1][j] - '0');
+			c = (prod / 10);
+			res[(i + j) + 1] += (prod % 10);
+			if (res[(i + j) + 1] > 9)
+			{
+				res[i + j] += res[(i + j) + 1] / 10;
+				res[(i + j) + 1] = res[(i + j) + 1] % 10;
+			}
+			res[(i + j) + 1] += c;
+		}
+	}
+
+	if (res[0] == 0)
+		i = 1;
 	else
-	{ numero(argv[1], argv[2]);
-		t1 = tama(argv[1]);
-		t2 = tama(argv[2]);
-		ind = t1 + t2;
-		mg = grande(t1, t2);
-		if (t1 >= t2)
-		{ size1 = argv[1];
-			size2 = argv[2];
-			b = t1 - 1;
-			a = t2 - 1;
-			t3 = t1 + 1; }
-		else
-		{ size1 = argv[2];
-			size2 = argv[1];
-			b = t2 - 1;
-			a = t1 - 1;
-			t3 = t2 + 1; }
-		g = b;
-		p = parcial(t3);
-		j = b + 1;
-		for (; a >= 0; a--, ind--)
-		{ car = 0;
-			b = g;
-			j = g + 1;
-			setiar(p, t3);
-			for (; b >= 0; b--, j--)
-			{ aux = ((size2[a] - '0') * (size1[b] - '0'));
-				aux = aux + car;
-				p[j] = ((aux % 10) + '0');
-				car = aux / 10;	}
-			p[j] = (car + '0');
-			add(p, mg, (ind - 1), (t3 - 1)); }
-		impresion(mg, (t1 + t2)); }
+		i = 0;
+	for (; i < length; i++)
+		printf("%d", res[i]);
+
+	printf("\n");
+	free(res);
 	return (0);
 }
