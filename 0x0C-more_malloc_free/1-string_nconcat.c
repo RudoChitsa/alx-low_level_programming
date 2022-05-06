93a798d804864e5c88a90cba1b04257e0ca9e99d
RudoChitsa <47056287+RudoChitsa@users.noreply.github.com>
2022-05-06 14:19:56 +0200

    task 00

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,35 +10,34 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int cont1 = 0, cont2 = 0, i;
-	char *p = NULL;
+	unsigned int l1, i, e;
+	char *a;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
-	while (s1[cont1] != '\0')
-		cont1++;
-	while (s2[cont2] != '\0')
-		cont2++;
-	if (n >= cont2)
-		n = cont2;
-	p = malloc(cont1 + n + 1);
-	if (!p)
-		return (NULL);
-	for (i = 0; i < cont1; i++)
-	{
-		p[i] = s1[i];
-	}
-	for (i = cont1; i < (cont1 + n); i++)
-	{
-		p[i] = s2[i - cont1];
-	}
-	p[i] = '\0';
-	return (p);
 
+	if (s2 == NULL)
+		s2 = "";
+	l1 = 0;
+	while (s1[l1])
+		l1++;
+
+	a = malloc(sizeof(*a) * l1 + n + 1);
+
+	if (a == NULL)
+		return (NULL);
+
+	for (i = 0, e = 0; i < (l1 + n); i++)
+	{
+		if (i < l1)
+		{
+			a[i] = s1[i];
+		}
+		else
+		{
+			a[i] = s2[e++];
+		}
+	}
+	a[i] = '\0';
+	return (a);
 }
