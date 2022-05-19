10f0f8197e94b16fb49faab27274c25b17084ba2
RudoChitsa <47056287+RudoChitsa@users.noreply.github.com>
2022-05-19 20:14:47 +0200

    task 04

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,8 +1,11 @@
-#include "lists.h""
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
 
 /**
  * _strlen - finds the length of a string
  * @str: string to find the length of
+ *
  * Return: length of string
  */
 unsigned int _strlen(char *str)
