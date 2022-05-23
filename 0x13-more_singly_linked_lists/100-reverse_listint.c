1fc60032730a8a37167dee98458dc8a25ee55c69
RudoChitsa <47056287+RudoChitsa@users.noreply.github.com>
2022-05-23 14:45:58 +0200

    task 11

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -6,7 +6,7 @@
 /**
  * reverse_listint - reverses a listint_t linked list.
  * @head: double pointer to the start of the list
- *
+ * reverse
  * Return: a pointer to the first node of the reversed list
  */
 listint_t *reverse_listint(listint_t **head)
