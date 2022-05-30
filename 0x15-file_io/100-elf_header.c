db7adc50e727a6950fdfe09a8511182c53fdeb05
RudoChitsa <47056287+RudoChitsa@users.noreply.github.com>
2022-05-30 14:39:00 +0200

    Update 100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,12 +1,4 @@
 #include "main.h"
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdlib.h>
-
-#define MAXSIZE 1204
-#define SE STDERR_FILENO
 
 /**
  * main - create the copy bash script
